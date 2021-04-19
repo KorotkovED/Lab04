c6683d8d37eaaa022fd8a028ae5fe7843905b26e
u211-05 <u211-05@public.mpei.local>
2021-04-19 10:13:45 +0300

    code: lab031

--- main.cpp
+++ main.cpp
@@ -1,48 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+
 
 using namespace std;
 
-int
-main() {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
-    }
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
-
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
-
-    // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
+void show_histogram_text(const vector<size_t>& bins)
+{
+ const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
     size_t max_count = 0;
@@ -73,6 +38,63 @@ main() {
         }
         cout << '\n';
     }
+}
 
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
+
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
+    }
+
+}
+
+int make_histogram(const vector<double>& numbers, const double min, const double max, size_t bin_count, vector<size_t>& bins)
+{
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+}
+
+int main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+const auto numbers = input_numbers(number_count);
+
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+double min, max;
+find_minmax(numbers, min, max);
+cout<<"min= "<< min<<endl;
+cout<<"max= "<< max<<endl;
+
+    vector<size_t> bins(bin_count);
+make_histogram(numbers,min, max, bin_count, bins);
+for ( int i = 0; i< bin_count; i++)
+{
+    cout<<" "<< bins[i]<<endl;
+}
+show_histogram_text(bins);
     return 0;
 }
