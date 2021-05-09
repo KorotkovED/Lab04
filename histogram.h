780e0331059e4726aaea3034945c1e7e4706efea
Alice <Alice@gmail.com>
2021-05-09 19:20:02 +0300

    code: testAndMainProgaram

---
+++ histogram.h
@@ -0,0 +1,8 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#include <vector>
+#define HISTOGRAM_H_INCLUDED
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+
+#endif // HISTOGRAM_H_INCLUDED
