2d353a9420f8ac27ba48ff5d97e41ef0ba3490a1
u211-05 <u211-05@public.mpei.local>
2021-05-31 11:19:29 +0300

    code: histogramh

--- histogram.h
+++ histogram.h
@@ -1,6 +1,8 @@
 #ifndef HISTOGRAM_H_INCLUDED
 #include <vector>
 #define HISTOGRAM_H_INCLUDED
+#include <string>
+#include <iostream>
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max);
@@ -14,4 +16,6 @@ void svg_begin(double width, double height);
 void svg_text(double left, double baseline, string text);
 
 void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black");
+struct Input;
+
 #endif // HISTOGRAM_H_INCLUDED
