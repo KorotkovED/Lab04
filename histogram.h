aed10a9ede0f398081cd03d3a31d9e3eca7317d5
Alice <Alice@gmail.com>
2021-06-05 19:40:22 +0300

    code:h1

--- histogram.h
+++ histogram.h
@@ -1,10 +1,10 @@
 #ifndef HISTOGRAM_H_INCLUDED
 #include <vector>
-#define HISTOGRAM_H_INCLUDED
 #include <string>
-#include <iostream>
+#define HISTOGRAM_H_INCLUDED
 using namespace std;
-
+struct Input {  vector<double> numbers; size_t bin_count;};
+    string make_info_text();
 void find_minmax(const vector<double>& numbers, double& min, double& max);
 
 void show_histogram_svg(const vector<size_t>& bins);
@@ -16,7 +16,4 @@ void svg_begin(double width, double height);
 void svg_text(double left, double baseline, string text);
 
 void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black");
-struct Input;
-
-string make_info_text();
 #endif // HISTOGRAM_H_INCLUDED
